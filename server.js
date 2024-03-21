bb3e3929498ed4a679b8270c70fbd63f983a13c0
Angel Montes <kamus_angel88@hotmail.com>
2024-03-20 22:59:47 -0600

    Levantar server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 import { connect } from "./config.js";
 connect();
+
+const api = express();
+//Creamos un objeto de express 
+api.use(express.json());
+//Habilitamos el uso de json
+
+api.listen(8000, ()=>{
+    console.log('Api corriendo en el puerto 8000')
+});
+
+
+
