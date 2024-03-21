f9139be89412ef6761958ffd5f1e4542a862644d
Angel Montes <kamus_angel88@hotmail.com>
2024-03-21 00:03:35 -0600

    Crear pelicula con validador de campos

--- models/Pelicula.js
+++ models/Pelicula.js
@@ -1,12 +1,30 @@
 import mongoose from "mongoose";
 
 const peliculaSchema = new mongoose.Schema({
-    nombre: String,
-    director: String,
-    fechaDeLanzamiento: Number, 
-    calificacion: String,
-    duracion: Number,
-    genero: String
+    nombre:{
+        type: String,
+        required: true,
+      },
+      director:{
+        type: String,
+        required: true,
+    },
+    fechaDeLanzamiento:{
+        type: Number,
+        required: true,
+    },
+    calificacion:{
+        type: String,
+        required: true,
+    },
+    duracion:{
+        type: Number,
+        required: true,
+    },
+    genero:{
+        type: String,
+        required: true,
+    },
 })
 
 export default mongoose.model('Pelicula', peliculaSchema);
